7e21b387a1317be03aafc43ad60d76516934377f
Fouad CHENNOUF <fouad@myem.fr>
2021-11-11 12:02:34 +0100

    using fixture factory instead of parametrize

--- myem_lib/pytest_fixtures.py
+++ myem_lib/pytest_fixtures.py
@@ -29,34 +29,43 @@ def integration_config(load_yml, rabbit_config):
     yield
 
 
+
+# for this fixture, we tried different solutions
+# we already tried with parametrize https://docs.pytest.org/en/6.2.x/fixture.html#fixture-parametrize
+# the probleme is that this solution cannot be wrapped in another fixture (cannot use pamaetrize for a fixture)
+# So, we decided to use factories as fixture
+# https://docs.pytest.org/en/latest/how-to/fixtures.html#factories-as-fixtures
 @pytest.fixture(scope="module")
-def db_dependency(load_yml, request):
+def db_dependency_factory(load_yml, request):
     # Do not import testing at the top, otherwise it will create problems with request lib
     # https://github.com/nameko/nameko/issues/693
     # https://github.com/gevent/gevent/issues/1016#issuecomment-328529454
     from nameko.testing.utils import get_extension
-    # https://stackoverflow.com/questions/18011902/pass-a-parameter-to-a-fixture-function
-    declarative_base = request.param
 
-    class Service:
-        name = "db_load_service"
+    def make_db_dependency(DeclarativeBase):
+        class Service:
+            name = "db_load_service"
 
-        db = DatabaseSession(declarative_base)
-        from nameko.testing.services import dummy
+            db = DatabaseSession(DeclarativeBase)
+            from nameko.testing.services import dummy
 
-        @dummy
-        def dummy(self):
-            pass
+            @dummy
+            def dummy(self):
+                pass
 
-    service = ServiceContainer(Service)
-    provider = get_extension(service, DatabaseSession)
-    # simulate db uris with the first existing one from yml file.
-    provider.container.config[DB_URIS_KEY]["db_load_service:Base"] = list(
-        provider.container.config[DB_URIS_KEY].values()
-    )[0]
-    provider.setup()
-    declarative_base.metadata.drop_all(provider.engine)
-    declarative_base.metadata.create_all(provider.engine)
-    yield provider.get_dependency(Mock())
-    declarative_base.metadata.drop_all(provider.engine)
-    provider.stop()
+        service = ServiceContainer(Service)
+        provider = get_extension(service, DatabaseSession)
+        # simulate db uris with the first existing one from yml file.
+        provider.container.config[DB_URIS_KEY]["db_load_service:Base"] = list(
+            provider.container.config[DB_URIS_KEY].values()
+        )[0]
+        provider.setup()
+        DeclarativeBase.metadata.drop_all(provider.engine)
+        DeclarativeBase.metadata.create_all(provider.engine)
+
+        def clean():
+            DeclarativeBase.metadata.drop_all(provider.engine)
+            provider.stop()
+        request.addfinalizer(clean)
+        return provider.get_dependency(Mock())
+    yield make_db_dependency
