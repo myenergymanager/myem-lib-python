84b73995b004ec5afdfb3c39fffad1e62c58edf5
smail <smail@myem.fr>
2021-11-24 16:32:24 +0100

    update HttpEntrypoint class

--- myem_lib/utils.py
+++ myem_lib/utils.py
@@ -32,6 +32,51 @@ class HttpEntrypoint(HttpRequestHandler):
             return response
         return HttpRequestHandler.response_from_exception(self, exc)
 
+    def __init__(self, method, url, expected_exceptions=(), **kwargs):
+        super().__init__(method, url, expected_exceptions=expected_exceptions)
+        self.allowed_origin = kwargs.get('origin', ['*'])
+        self.allowed_methods = kwargs.get('methods', ['*'])
+        self.allow_credentials = kwargs.get('credentials', True)
+
+    def handle_request(self, request):
+        self.request = request
+        if request.method == 'OPTIONS':
+            return self.response_from_result(result='')
+        return super().handle_request(request)
+
+    def response_from_result(self, *args, **kwargs):
+        response = super(HttpEntrypoint, self).response_from_result(*args, **kwargs)
+        response.headers.add("Access-Control-Allow-Headers",
+                             self.request.headers.get("Access-Control-Request-Headers"))
+        response.headers.add("Access-Control-Allow-Credentials", str(self.allow_credentials).lower())
+        response.headers.add("Access-Control-Allow-Methods", ",".join(self.allowed_methods))
+        response.headers.add("Access-Control-Allow-Origin", ",".join(self.allowed_origin))
+        return response
+
+    @classmethod
+    def decorator(cls, *args, **kwargs):
+        """
+        We're overriding the decorator classmethod to allow it to register an options
+        route for each standard REST call. This saves us from manually defining OPTIONS
+        routes for each CORs enabled endpoint
+        """
+        def registering_decorator(fn, args, kwargs):
+            instance = cls(*args, **kwargs)
+            register_entrypoint(fn, instance)
+            if instance.method in ('GET', 'POST', 'DELETE', 'PUT', 'PATCH') and \
+                    ('*' in instance.allowed_methods or instance.method in instance.allowed_methods):
+                options_args = ['OPTIONS'] + list(args[1:])
+                options_instance = cls(*options_args, **kwargs)
+                register_entrypoint(fn, options_instance)
+            return fn
+
+        if len(args) == 1 and isinstance(args[0], types.FunctionType):
+            return registering_decorator(args[0], args=(), kwargs={})
+        else:
+            return partial(registering_decorator, args=args, kwargs=kwargs)
+        # more https://github.com/nameko/nameko/issues/309
+        # https://github.com/harel/nameko-cors/edit/master/__init__.py
+
 
 http = HttpEntrypoint.decorator
 
