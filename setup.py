aeb7588cfe634ec9f4d8475a7add0e8242f1701b
khaldi-abdarhmane <57477274+khaldi-abdarhmane@users.noreply.github.com>
2023-04-27 09:56:49 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "psycopg2-binary>=2.9.1",
         "nameko>=3.0.0-rc10",
         "jwcrypto",
-        "fastapi_pagination>=0.9.1",
+        "fastapi_pagination==0.9.1",
     ],
     extras_require={
         "dev": [
