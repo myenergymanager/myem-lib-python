edd0870eeedc6c581b5a88d8b0d407e0536fc0b5
Youcef-Abdelliche <youcef.a@myem.fr>
2023-05-24 13:56:04 +0100

    MYEM-4464 #time 5m #comment update pool_size and max_overflow

--- myem_lib/db_settings_mixins.py
+++ myem_lib/db_settings_mixins.py
@@ -47,7 +47,7 @@ class DbSettingsMixin:
             "keepalives_count": 5,
         },
         pool_size=20,
-        max_overflow=10,
+        max_overflow=25,
         pool_pre_ping=True,
     )
     session_maker = sessionmaker(bind=engine)
